58cb958e16bbbfa7c09dac08122368d6bc944bc5
elmambou <esmambou@gmail.com>
2024-01-26 22:50:30 +0200

    clearing files for lesson 03 assignment

--- routes/index1.js
+++
@@ -1,9 +0,0 @@
-// Application endpoints index
-
-const express = require('express');
-const router = express.Router();
-
-router.use('./contacts', require('./contacts'))
-
-
-module.exports = router;
